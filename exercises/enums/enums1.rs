7d2251a2c44c7bc3f10b66b10698811b47eb399a
FleetingGlimps <147240877+FleetingGlimps@users.noreply.github.com>
2025-04-11 16:22:22 +0000

    enum

--- exercises/enums/enums1.rs
+++ exercises/enums/enums1.rs
@@ -1,12 +1,15 @@
+// I AM NOT DONE
 // enums1.rs
 //
 // No hints this time! ;)
 
-// I AM NOT DONE
-
 #[derive(Debug)]
 enum Message {
     // TODO: define a few types of messages as used below
+    Quit,
+    Echo(String),
+    Move{x:i32,y:i32},
+    ChangeColor(u8,u8,u8),
 }
 
 fn main() {
